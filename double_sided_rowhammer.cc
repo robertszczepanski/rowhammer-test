bf80bd67b7b0d3a49fc3893aff5d0c755836f0b3
robertszczepanski <robert.szczepanski.it@gmail.com>
2023-01-15 18:23:36 +0100

    Convert to 32-bit architecture without floating point operations

--- double_sided_rowhammer.cc
+++ double_sided_rowhammer.cc
@@ -50,35 +50,34 @@
 namespace {
 
 // The fraction of physical memory that should be mapped for testing.
-double fraction_of_physical_memory = 0.3;
+uint32_t fraction_of_physical_memory = 10;
 
 // The time to hammer before aborting. Defaults to one hour.
-uint64_t number_of_seconds_to_hammer = 3600;
+uint32_t number_of_seconds_to_hammer = 3600;
 
 // The number of memory reads to try.
-uint64_t number_of_reads = 1000*1024;
+uint32_t number_of_reads = 1000*1024;
 
 // Obtain the size of the physical memory of the system.
-uint64_t GetPhysicalMemorySize() {
+uint32_t GetPhysicalMemorySize() {
   struct sysinfo info;
   sysinfo( &info );
+  printf("[!] Total RAM: %d, Unit: %d\n", (size_t)info.totalram, (size_t)info.mem_unit);
   return (size_t)info.totalram * (size_t)info.mem_unit;
 }
 
-uint64_t GetPageFrameNumber(int pagemap, uint8_t* virtual_address) {
+uint32_t GetPageFrameNumber(int pagemap, uint8_t* virtual_address) {
   // Read the entry in the pagemap.
-  uint64_t value;
-  int got = pread(pagemap, &value, 8,
-                  (reinterpret_cast<uintptr_t>(virtual_address) / 0x1000) * 8);
-  assert(got == 8);
-  uint64_t page_frame_number = value & ((1ULL << 54)-1);
+  uint32_t value;
+  int got = pread(pagemap, &value, 4,
+                  (reinterpret_cast<uintptr_t>(virtual_address) / 0x1000) * 4);
+  assert(got == 4);
+  uint32_t page_frame_number = value & ((1ULL << 54)-1);
   return page_frame_number;
 }
 
-void SetupMapping(uint64_t* mapping_size, void** mapping) {
-  *mapping_size = 
-    static_cast<uint64_t>((static_cast<double>(GetPhysicalMemorySize()) * 
-          fraction_of_physical_memory));
+void SetupMapping(uint32_t* mapping_size, void** mapping) {
+  *mapping_size = GetPhysicalMemorySize() / fraction_of_physical_memory;
 
   *mapping = mmap(NULL, *mapping_size, PROT_READ | PROT_WRITE,
       MAP_POPULATE | MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
@@ -86,82 +85,86 @@ void SetupMapping(uint64_t* mapping_size, void** mapping) {
 
   // Initialize the mapping so that the pages are non-empty.
   printf("[!] Initializing large memory mapping ...");
-  for (uint64_t index = 0; index < *mapping_size; index += 0x1000) {
-    uint64_t* temporary = reinterpret_cast<uint64_t*>(
+  for (uint32_t index = 0; index < *mapping_size; index += 0x1000) {
+    uint32_t* temporary = reinterpret_cast<uint32_t*>(
         static_cast<uint8_t*>(*mapping) + index);
     temporary[0] = index;
   }
   printf("done\n");
 }
 
-uint64_t HammerAddressesStandard(
-    const std::pair<uint64_t, uint64_t>& first_range,
-    const std::pair<uint64_t, uint64_t>& second_range,
-    uint64_t number_of_reads) {
-  volatile uint64_t* first_pointer =
-      reinterpret_cast<uint64_t*>(first_range.first);
-  volatile uint64_t* second_pointer =
-      reinterpret_cast<uint64_t*>(second_range.first);
-  uint64_t sum = 0;
+uint32_t HammerAddressesStandard(
+    const std::pair<uint32_t, uint32_t>& first_range,
+    const std::pair<uint32_t, uint32_t>& second_range,
+    uint32_t number_of_reads) {
+  volatile uint32_t* first_pointer =
+      reinterpret_cast<uint32_t*>(first_range.first);
+  volatile uint32_t* second_pointer =
+      reinterpret_cast<uint32_t*>(second_range.first);
+  uint32_t sum = 0;
 
-  volatile uint64_t* cache_pointer;
+  volatile uint32_t* cache_pointer;
   while (number_of_reads-- > 0) {
     sum += first_pointer[0];
     sum += second_pointer[0];
 
-    if (first_pointer > reinterpret_cast<uint64_t*>(0x20000)) {
-      cache_pointer = reinterpret_cast<uint64_t*>(0x10000);
+    if (first_pointer > reinterpret_cast<uint32_t*>(0x20000)) {
+      cache_pointer = reinterpret_cast<uint32_t*>(0x10000);
     } else {
-      cache_pointer = reinterpret_cast<uint64_t*>(0x30000);
+      cache_pointer = reinterpret_cast<uint32_t*>(0x30000);
     }
+    printf("Clear cache...\n");
     for (int i = 0; i < 4096; i++) {
       asm volatile(
-        "" : : "r" (cache_pointer++) : "memory");
+        "lw t0, (%0)\n\t" : : "r" (cache_pointer++) : "memory");
     }
+    printf("Attack memory\n");
     asm volatile(
-        "" : : "r" (first_pointer), "r" (second_pointer) : "memory");
+        "lw t0, (%0)\n\t;"
+        "lw t0, (%1)\n\t;"
+        : : "r" (first_pointer), "r" (second_pointer) : "memory");
   }
   return sum;
 }
 
-typedef uint64_t(HammerFunction)(
-    const std::pair<uint64_t, uint64_t>& first_range,
-    const std::pair<uint64_t, uint64_t>& second_range,
-    uint64_t number_of_reads);
+typedef uint32_t(HammerFunction)(
+    const std::pair<uint32_t, uint32_t>& first_range,
+    const std::pair<uint32_t, uint32_t>& second_range,
+    uint32_t number_of_reads);
 
 // A comprehensive test that attempts to hammer adjacent rows for a given 
 // assumed row size (and assumptions of sequential physical addresses for 
 // various rows.
-uint64_t HammerAllReachablePages(uint64_t presumed_row_size, 
-    void* memory_mapping, uint64_t memory_mapping_size, HammerFunction* hammer,
-    uint64_t number_of_reads) {
+uint32_t HammerAllReachablePages(uint32_t presumed_row_size,
+    void* memory_mapping, uint32_t memory_mapping_size, HammerFunction* hammer,
+    uint32_t number_of_reads) {
   // This vector will be filled with all the pages we can get access to for a
   // given row size.
   std::vector<std::vector<uint8_t*>> pages_per_row;
-  uint64_t total_bitflips = 0;
+  uint32_t total_bitflips = 0;
 
   pages_per_row.resize(memory_mapping_size / presumed_row_size);
   int pagemap = open("/proc/self/pagemap", O_RDONLY);
   assert(pagemap >= 0);
 
-  printf("[!] Identifying rows for accessible pages ... ");
-  for (uint64_t offset = 0; offset < memory_mapping_size; offset += 0x1000) {
+  printf("[!] Identifying rows for accessible pages ...\n ");
+  for (uint32_t offset = 0; offset < memory_mapping_size; offset += 0x1000) {
     uint8_t* virtual_address = static_cast<uint8_t*>(memory_mapping) + offset;
-    uint64_t page_frame_number = GetPageFrameNumber(pagemap, virtual_address);
-    uint64_t physical_address = page_frame_number * 0x1000;
-    uint64_t presumed_row_index = physical_address / presumed_row_size;
-    //printf("[!] put va %lx pa %lx into row %ld\n", (uint64_t)virtual_address,
-    //    physical_address, presumed_row_index);
+    uint32_t page_frame_number = GetPageFrameNumber(pagemap, virtual_address);
+    uint32_t physical_address = page_frame_number * 0x1000;
+    uint32_t presumed_row_index = physical_address / presumed_row_size;
+    printf("[!] put va %lx pa %lx into row %ld\n", (uint32_t)virtual_address,
+       physical_address, presumed_row_index);
     if (presumed_row_index > pages_per_row.size()) {
       pages_per_row.resize(presumed_row_index);
     }
     pages_per_row[presumed_row_index].push_back(virtual_address);
-    //printf("[!] done\n");
+    printf("[!] done\n");
   }
   printf("Done\n");
 
   // We should have some pages for most rows now.
-  for (uint64_t row_index = 0; row_index + 2 < pages_per_row.size(); 
+  for (uint32_t row_index = 0; row_index + 2 < pages_per_row.size();
       ++row_index) {
     if ((pages_per_row[row_index].size() != 64) || 
         (pages_per_row[row_index+2].size() != 64)) {
@@ -188,15 +191,15 @@ uint64_t HammerAllReachablePages(uint64_t presumed_row_size,
           memset(target_page, 0xFF, 0x1000);
         }
         // Now hammer the two pages we care about.
-        std::pair<uint64_t, uint64_t> first_page_range(
-            reinterpret_cast<uint64_t>(first_row_page), 
-            reinterpret_cast<uint64_t>(first_row_page+0x1000));
-        std::pair<uint64_t, uint64_t> second_page_range(
-            reinterpret_cast<uint64_t>(second_row_page),
-            reinterpret_cast<uint64_t>(second_row_page+0x1000));
+        std::pair<uint32_t, uint32_t> first_page_range(
+            reinterpret_cast<uint32_t>(first_row_page),
+            reinterpret_cast<uint32_t>(first_row_page+0x1000));
+        std::pair<uint32_t, uint32_t> second_page_range(
+            reinterpret_cast<uint32_t>(second_row_page),
+            reinterpret_cast<uint32_t>(second_row_page+0x1000));
         hammer(first_page_range, second_page_range, number_of_reads);
         // Now check the target pages.
-        uint64_t number_of_bitflips_in_target = 0;
+        uint32_t number_of_bitflips_in_target = 0;
         for (const uint8_t* target_page : pages_per_row[row_index+1]) {
           for (uint32_t index = 0; index < 0x1000; ++index) {
             if (target_page[index] != 0xFF) {
@@ -219,8 +222,8 @@ uint64_t HammerAllReachablePages(uint64_t presumed_row_size,
   return total_bitflips;
 }
 
-void HammerAllReachableRows(HammerFunction* hammer, uint64_t number_of_reads) {
-  uint64_t mapping_size;
+void HammerAllReachableRows(HammerFunction* hammer, uint32_t number_of_reads) {
+  uint32_t mapping_size;
   void* mapping;
   SetupMapping(&mapping_size, &mapping);
 
